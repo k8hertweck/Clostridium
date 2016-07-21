3ccf79f14a66b369b1986130a8e2cc4bfa254d61
k8hertweck <k8hertweck@gmail.com>
2016-07-21 12:43:47 -0500

    initial QC script

---
+++ rawQC.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+## quality control of raw sequence data prior to assembly 
+## usage:
+#	rawQC.sh PATH/TO/DATA
+## dependencies:
+#	fastqc
+#	trimmomatic
+
+TRIM=/Applications/Trimmomatic-0.36
+DATA=$1
+SCRIPTS=`pwd`
+
+cd $DATA
+
+## QC of raw files
+fastqc *.fastq.gz
+mkdir raw
+mv *zip *html *gz raw
+
+## quality trimming and filtering
+# make list of files
+ls *.gz | sed s/_R[12]_001.fastq.gz// | uniq > $SCRIPTS/ClostridiumStrains.lst
+# set up directory
+mkdir trim
+# loop across all strains
+for x in `cat $SCRIPTS/ClostridiumStrains.lst`
+	do
+		# quality trimming 
+		java -jar $TRIM/trimmomatic-0.36.jar PE -threads 4 -phred33 \
+			$DATA/raw/"$x"_R1_001.fastq.gz $DATA/raw/"$x"_R2_001.fastq.gz \
+			$DATA/trim/"$x"_1paired.fq.gz $DATA/trim/"$x"_1unpaired.fq.gz \
+			$DATA/trim/"$x"_2paired.fq.gz $DATA/trim/"$x"_2unpaired.fq.gz \
+			ILLUMINACLIP:$TRIM/adapters/TruSeq3-PE.fa:2:30:10 SLIDINGWINDOW:4:20 LEADING:15 TRAILING:15  MINLEN:50
+done
+
+# reassess trimmed files
+cd trim
+fastqc *_1paired.fq.gz *_2paired.fq.gz
