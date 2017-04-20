76549688f503a4b6edeb683b5556c27e6e16d3e0
k8hertweck <k8hertweck@gmail.com>
2017-04-20 14:46:24 -0500

    placeholder for mapping

---
+++ mapping.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## read mapping of Clostridium strains
+## usage: mapping.sh PATH/TO/WORKING
+## dependencies
+#	
+
+CLOS=$1
+SCRIPTS=`pwd`
+
+cd $CLOS
+
+# index references
+
+# map references
+
+# variant calling 
