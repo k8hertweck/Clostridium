e86daf3de0875aaa606a3c2e7b3d38a3835e5740
k8hertweck <k8hertweck@gmail.com>
2018-08-30 17:18:35 -0700

    updating to correct final combining files

--- rawQC.sh
+++ rawQC.sh
@@ -6,28 +6,29 @@
 # 	PATH/TO/PROJECT is location of directory containing all fastq.gz files
 # 	set TRIM to wherever Trimmomatic is installed
 ## dependencies:
-#	fastqc (v0.11.7): quality control 
+#	fastqc (v0.11.7): quality control
 #		(https://www.bioinformatics.babraham.ac.uk/projects/fastqc/), installed and in path
 #	trimmomatic (version 0.38): quality filtering and trimming
 #		(http://www.usadellab.org/cms/?page=trimmomatic)
 
-TRIM=/Applications/Trimmomatic-0.38
 PROJECT=$1
+FASTQC=/Applications/FastQC-0.11.7/fastqc
+TRIM=/Applications/Trimmomatic-0.38
 SCRIPTS=`pwd`
 
 cd $PROJECT
 
 ## QC of raw files
 echo "FASTQC ON RAW FILES"
-fastqc *.fastq.gz
+#$FASTQC *.fastq.gz
 mkdir raw
 mv *zip *html *gz raw
 
 ## quality trimming and filtering
 # make list of files
-ls raw/*.gz | sed s/_R[12]_001.fastq.gz// | sed s/raw.// | uniq > $SCRIPTS/ClostridiumFiles.lst
+#ls raw/*.gz | sed s/_R[12]_001.fastq.gz// | sed s/raw.// | uniq > $SCRIPTS/ClostridiumFiles.lst
 # make list of strains
-sed s/_S._L00[1234]// $SCRIPTS/ClostridiumFiles.lst > $SCRIPTS/ClostridiumStrains.lst
+#sed s/_S._L00[1234]// $SCRIPTS/ClostridiumFiles.lst > $SCRIPTS/ClostridiumStrains.lst
 # set up directory
 mkdir trim
 # loop trimmomatic across all strains
@@ -36,10 +37,10 @@ for x in `cat $SCRIPTS/ClostridiumFiles.lst`
 	do
 		# quality trimming
 		echo $x
-		java -jar $TRIM/trimmomatic-0.36.jar PE -threads 4 -phred33 \
-			$DATA/raw/"$x"_R1_001.fastq.gz $DATA/raw/"$x"_R2_001.fastq.gz \
-			$DATA/trim/"$x"_1paired.fq.gz $DATA/trim/"$x"_1unpaired.fq.gz \
-			$DATA/trim/"$x"_2paired.fq.gz $DATA/trim/"$x"_2unpaired.fq.gz \
+		java -jar $TRIM/trimmomatic-0.38.jar PE -threads 4 -phred33 \
+			$PROJECT/raw/"$x"_R1_001.fastq.gz $PROJECT/raw/"$x"_R2_001.fastq.gz \
+			$PROJECT/trim/"$x"_1paired.fq.gz $PROJECT/trim/"$x"_1unpaired.fq.gz \
+			$PROJECT/trim/"$x"_2paired.fq.gz $PROJECT/trim/"$x"_2unpaired.fq.gz \
 			ILLUMINACLIP:$TRIM/adapters/NexteraPE-PE.fa:2:30:10 \
 			SLIDINGWINDOW:4:15 LEADING:3 TRAILING:3  HEADCROP:8 MINLEN:50
 done
@@ -47,7 +48,7 @@ done
 # reassess trimmed files
 cd trim
 echo "FASTQC ON TRIMMED FILES"
-fastqc *_1paired.fq.gz *_2paired.fq.gz
+#$FASTQC *_1paired.fq.gz *_2paired.fq.gz
 
 # concatenate read files
 mkdir combined
@@ -55,6 +56,6 @@ echo "CONCATENATING TRIMMED FILES"
 for x in `cat $SCRIPTS/ClostridiumStrains.lst`
 	do
 		echo $x
-		cat trim/$x_L00*_1paired.fq.gz > combined/$x_R1.fq.gz
-		cat trim/$x_L00*_2paired.fq.gz > combined/$x_R2.fq.gz
+		cat trim/"$x"_L00*_1paired.fq.gz > combined/"$x"_R1.fq.gz
+		cat trim/"$x"_L00*_2paired.fq.gz > combined/"$x"_R2.fq.gz
 done
