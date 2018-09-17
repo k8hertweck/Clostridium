c4e2173a4496f9cc20d239d014bf016151747b06
k8hertweck <k8hertweck@gmail.com>
2018-09-17 08:26:49 -0700

    removing realignment

--- variant_calling.sh
+++ variant_calling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## variant calling for Clostridium strains 
+## variant calling for Clostridium strains
 ## usage: variant_calling.sh PATH/TO/PROJECT
 ## dependencies
 #   Picard-tools (v2.11.0): http://broadinstitute.github.io/picard
@@ -9,8 +9,6 @@
 ## pre-process reads
 # combine strain data
 MergeSamFiles (picard)
-# realign reads
-RealignerTargetCreator/IndelRealigner (gatk)
 # filter out reads with zero mapping quality and low base quality
 gatk
 
