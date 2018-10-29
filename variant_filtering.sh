7591f27e64e0ca44f83c6bce137543f51863fc76
k8hertweck <k8hertweck@gmail.com>
2018-10-29 10:15:06 -0700

    adding test notes

--- variant_filtering.sh
+++ variant_filtering.sh
@@ -4,11 +4,22 @@
 ## usage: variant_filtering.sh PATH/TO/PROJECT
 ## dependencies
 #   Picard-tools (v2.11.0): http://broadinstitute.github.io/picard
-#   GATK (v3.8.0): https://software.broadinstitute.org/gatk/
+#   GATK (vXXX): https://software.broadinstitute.org/gatk/
+
+PROJECT=$1
+SCRIPTS=`pwd`
+GATK=$HOME/bin/gatk/
+
+cd $SCRIPTS
 
 ## SNPs
 # exclude variants within 6 bases of an indel
-SelectVariants
+java -jar $GATK/gatk-package-4.0.8.1-local.jar \
+  SelectVariants \
+  -R $SCRIPTS/references/Cace-ATCC824-both.fasta \
+  -V variants/test.vcf.gz \
+  --select-type-to-include SNP
+  -O variants/filtered_test.vcf
 # exclude variants called as heterozygotes in diploid analysis
 SelectVariants
 # exclude non-reference calls
@@ -21,6 +32,7 @@ SelectVariants
 ## indels
 # exclude variants called as heterozygotes in diploid analysis
 SelectVariants
+--select-type-to-include INDEL
 # exclude non-reference calls
 SelectVariants
 # exclude sites with extremely high depths of coverage
