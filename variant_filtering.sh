6f2ed522738295f3e02a7d97b556a1e1e65ed6c3
k8hertweck <k8hertweck@gmail.com>
2018-11-04 20:22:18 -0800

    adding filtering notes

--- variant_filtering.sh
+++ variant_filtering.sh
@@ -11,23 +11,39 @@ GATK=$HOME/bin/gatk
 
 cd $SCRIPTS
 
-# extract heterozygotes from diploid analysis
+# identify heterozygotes from diploid analysis
 java -jar $GATK/gatk-package-4.0.8.1-local.jar \
   VariantFiltration \
   -R $SCRIPTS/references/Cace-ATCC824-both.fasta \
   -V variants/Cace-variants_diploid.vcf.gz \
   --genotype-filter-expression "isHet == 1" \ # identify heterozygous variants
   --genotype-filter-name "isHetFilter" \ # label heterozygous variants
-  -O variants/test_diploid.vcf
+  -O variants/het_diploid_marked.vcf
+# extract heterozygotes from diploid analysis
+java -jar $GATK/gatk-package-4.0.8.1-local.jar \
+  SelectVariants \
+  -R $SCRIPTS/references/Cace-ATCC824-both.fasta \
+  -V variants/Cace-variants_diploid.vcf.gz \
+  -select "isHet == 1" \
+  -O variants/het_diploid_exclude.vcf
+# exclude heterozygotes from diploid analysis
+java -jar GenomeAnalysisTK.jar \
+   -R $SCRIPTS/references/Cace-ATCC824-both.fasta \
+   -T SelectVariants \
+   --variant input.vcf \
+   -o output.vcf \
+   -excludeIDs fileExclude
+
 
 ## SNPs
-# annotate SNPs to remove
+# extract just SNPs from callset
 java -jar $GATK/gatk-package-4.0.8.1-local.jar \
   VariantFiltration \
   -R $SCRIPTS/references/Cace-ATCC824-both.fasta \
-  -V variants/test.vcf.gz \
+  -V variants/XXX.vcf \
   --select-type-to-include SNP
-  
+# exclude heterozygotes from diploid analysis
+
   # exclude variants within 6 bases of an indel
   # exclude non-reference calls
   # exclude sites with extremely high depths of coverage
@@ -38,15 +54,17 @@ java -jar $GATK/gatk-package-4.0.8.1-local.jar \
   SelectVariants \
   -V XXX.vcf \
   --set-filtered-gt-to-nocall \
-  -O output.vcf
+  -O Cace-snps.vcf
 
 ## indels
 # exclude variants called as heterozygotes in diploid analysis
-SelectVariants
---select-type-to-include INDEL
+java -jar $GATK/gatk-package-4.0.8.1-local.jar \
+  VariantFiltration \
+  -R $SCRIPTS/references/Cace-ATCC824-both.fasta \
+  -V variants/raw_indels.vcf.gz \
+  --select-type-to-include INDEL
 # exclude non-reference calls
-SelectVariants
+
 # exclude sites with extremely high depths of coverage
-SelectVariants
+
 # exclude variants in low-complexity regions
-SelectVariants
